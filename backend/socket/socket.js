771778390a400bd96f1f16b3a9064f6fb699e6d6
vishwadeep2510 <vishwadeepsingh1025@gmail.com>
2025-12-24 20:25:47 +0530

    fixed major bugs and introduced typing status

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -1,38 +1,53 @@
-import {Server} from "socket.io";
+import { Server } from "socket.io";
 import http from "http";
 import express from "express";
 
 const app = express();
-
 const server = http.createServer(app);
+
 const io = new Server(server, {
-    cors:{
-        origin:['http://localhost:3000'],
-        methods:['GET', 'POST'],
-    },
+  cors: {
+    origin: ["http://localhost:3000"],
+    methods: ["GET", "POST"],
+  },
 });
 
+const userSocketMap = {};
+
 export const getReceiverSocketId = (receiverId) => {
-    return userSocketMap[receiverId];
-}
+  return userSocketMap[receiverId];
+};
 
-const userSocketMap = {}; // {userId->socketId}
+io.on("connection", (socket) => {
+  const userId = socket.handshake.query.userId;
 
 
-io.on('connection', (socket)=>{
-    const userId = socket.handshake.query.userId
-    if(userId !== undefined){
-        userSocketMap[userId] = socket.id;
-    } 
+  if (userId !== undefined) {
+    userSocketMap[userId] = socket.id;
+  }
 
-    io.emit('getOnlineUsers',Object.keys(userSocketMap));
+  io.emit("getOnlineUsers", Object.keys(userSocketMap));
 
-    socket.on('disconnect', ()=>{
-        delete userSocketMap[userId];
-        io.emit('getOnlineUsers',Object.keys(userSocketMap));
-    })
 
-})
+  socket.on("typing", ({ senderId, receiverId }) => {
+    const receiverSocketId = getReceiverSocketId(receiverId);
+    if (receiverSocketId) {
+      io.to(receiverSocketId).emit("typing", { senderId });
+    }
+  });
 
-export {app, io, server};
+  socket.on("stopTyping", ({ senderId, receiverId }) => {
+    const receiverSocketId = getReceiverSocketId(receiverId);
+    if (receiverSocketId) {
+      io.to(receiverSocketId).emit("stopTyping", { senderId });
+    }
+  });
 
+
+  socket.on("disconnect", () => {
+    delete userSocketMap[userId];
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+  });
+});
+
+export { app, io, server };
