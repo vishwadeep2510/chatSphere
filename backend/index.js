424ad8009e327a1a3d189273f7ace1b2afa12bc4
vishwadeep2510 <vishwadeepsingh1025@gmail.com>
2025-12-25 18:39:08 +0530

    fixed cors error

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json()); 
 app.use(cookieParser());
 const corsOption={
-    origin:'https://chatsphere-5u9g.onrender.com',
+    origin:'http://localhost:3000/',
     credentials:true
 };
 app.use(cors(corsOption)); 
