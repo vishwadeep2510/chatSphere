9bbec7d3686caa48b1d71a7161a49c04091a03cc
vishwadeep2510 <vishwadeepsingh1025@gmail.com>
2025-12-25 20:05:40 +0530

    fixed getOtherUsers

--- frontend/src/hooks/useGetOtherUsers.jsx
+++ frontend/src/hooks/useGetOtherUsers.jsx
@@ -1,24 +1,25 @@
 import { useEffect } from 'react';
 import axios from "axios";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { setOtherUsers } from '../redux/userSlice';
 import { BASE_URL } from '..';
 
 const useGetOtherUsers = () => {
     const dispatch = useDispatch();
-
+    const {authUser} = useSelector(store=>store.user);
     useEffect(() => {
+        if(!authUser)return;
         const fetchOtherUsers = async () => {
             try {
                 axios.defaults.withCredentials = true;
                 const res = await axios.get(`${BASE_URL}/api/v1/user`);
                 dispatch(setOtherUsers(res.data));
             } catch (error) {
-                console.log(error);
+                console.log("failed to fetch users", error);
             }
         }
         fetchOtherUsers();
-    }, [dispatch])
+    }, [authUser, dispatch])
 
 }
 
