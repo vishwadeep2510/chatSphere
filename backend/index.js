46a183d48ab59166a150bdbf47bf96a061643308
vishwadeep2510 <vishwadeepsingh1025@gmail.com>
2025-12-25 19:10:16 +0530

    fixed cors error

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json()); 
 app.use(cookieParser());
 const corsOption={
-    origin:'http://localhost:3000/',
+    origin:'http://localhost:3000',
     credentials:true
 };
 app.use(cors(corsOption)); 
